f8bec177cb989844cabc86b1cd526b2499ea22df
Fredi Vodounoupke <fvodounoupke@gmail.com>
2026-04-24 20:16:39 +0100

    [ADD] ajout du main.c

--- HUNTER_bird/src/main.c
+++ HUNTER_bird/src/main.c
@@ -1,7 +1,33 @@
+/*
+========================================
+HUNTER BIRD
+Fichier  : src/main.c
+Role     : Point d'entree du programme
+Auteur   : Dev A
+========================================
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
-int main()
+#include "game.h"
+
+int main(void)
 {
-    printf("Hello world!\n");
-    return 0;
+    Game game = {0};
+
+    /* Etape 1 : Initialiser SDL2 et creer la fenetre */
+    if (!init_game(&game)) {
+        printf("Erreur : impossible de lancer le jeu\n");
+        cleanup(&game);
+        return EXIT_FAILURE;
+    }
+
+    /* Etape 2 : Lancer la boucle de jeu */
+    printf("Jeu lance ! Appuie sur Echap pour quitter\n");
+    game_loop(&game);
+
+    /* Etape 3 : Nettoyer la memoire */
+    cleanup(&game);
+
+    return EXIT_SUCCESS;
 }
